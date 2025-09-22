31eaab5366d30f809e9208cc743d4a406a349e82
Hampus von Horn <hampus.vh@gmail.com>
2025-09-22 11:43:49 +0200

    added auth routes for login and register

--- src/routes/auth_routes.js
+++ src/routes/auth_routes.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
-import { login } from "../controllers/auth_controller.js";
+import { login, register } from "../controllers/auth_controller.js";
 
 const router = Router();
 
 router.post("/login", login);
+router.post("/register", register);
 
 export default router;
