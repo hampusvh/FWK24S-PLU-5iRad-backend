135208e2c226d7b366d33d713f3a6b89ad0cbc83
Hampus von Horn <hampus.vh@gmail.com>
2025-09-22 10:43:20 +0200

    chore(app): implemented auth route in app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import healthRoutes from "./routes/health_routes.js";
 import gomokuRoutes from "./routes/gomoku_routes.js";
+import authRoutes from "./routes/auth_routes.js";
 
 const app = express();
 
@@ -8,6 +9,7 @@ app.use(express.json());
 
 app.use("/api/health", healthRoutes);
 app.use("/api/gomoku", gomokuRoutes);
+app.use("/api/auth", authRoutes);
 
 app.use((req, res) => {
   res.status(404).json({
