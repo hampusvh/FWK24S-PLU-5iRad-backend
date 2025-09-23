47fea01bbbfd60f0445e87cbd067d7d457d5b524
angelika-friis <angelikafriis@gmail.com>
2025-09-23 22:50:51 +0200

    changed url endpoint to kebab case

--- src/routes/gomoku_routes.js
+++ src/routes/gomoku_routes.js
@@ -3,10 +3,10 @@ import { add_token, create_game, add_player, get_tiles, get_game } from "../cont
 
 const router = Router();
 
-router.post("/add_token", add_token);
-router.post("/create_game", create_game);
-router.post("/add_player", add_player);
-router.get("/get_tiles", get_tiles);
-router.get("/get_game", get_game);
+router.post("/add-token", add_token);
+router.post("/create-game", create_game);
+router.post("/add-player", add_player);
+router.get("/get-tiles", get_tiles);
+router.get("/get-game", get_game);
 
 export default router;
