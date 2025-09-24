b1cd79311743ca0e670f68d2780a5ef904e7193d
Axel Lindgren Wenell <axel.lindgren.wenell@hotmail.se>
2025-09-24 09:14:46 +0200

    applied auth middleware to /api/gomoku

--- src/app.js
+++ src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import healthRoutes from "./routes/health_routes.js";
 import gomokuRoutes from "./routes/gomoku_routes.js";
 import authRoutes from "./routes/auth_routes.js";
+import { auth } from "./middleware/auth_middleware.js";
 
 const app = express();
 
@@ -15,7 +16,7 @@ app.use(cors({
 app.use(express.json());
 
 app.use("/api/health", healthRoutes);
-app.use("/api/gomoku", gomokuRoutes);
+app.use("/api/gomoku", auth, gomokuRoutes);
 app.use("/api/auth", authRoutes);
 
 app.use((req, res) => {
